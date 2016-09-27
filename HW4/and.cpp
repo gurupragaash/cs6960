5ea2ceee296937132ff0cb4c473c36f027bc86be
guru <u1010032@utah.edu>
2016-09-27 11:16:24 -0600

    Instead of recursion, using different functions and making them inline

--- HW4/and.cpp
+++ HW4/and.cpp
@@ -42,11 +42,75 @@ inline interval joinIntervals(interval a, interval b)
   return interval(lo, hi);
 }
 
-interval interval::operator&(const interval &other) {
-  int lowest = interval::MIN, highest = interval::MAX;
+inline interval andOper(const interval &first, const interval &other) {
+  int highest;
   int knownZerosThis, knownZerosOther;
   int knownOnesThis, knownOnesOther;
   int resultKnownOnes, resultKnownZeros;
+
+  knownBits(first, &knownOnesThis, &knownZerosThis);
+  knownBits(other, &knownOnesOther, &knownZerosOther);
+  resultKnownOnes = knownOnesThis & knownOnesOther; 
+  resultKnownZeros = knownZerosThis | knownZerosOther;
+  
+  //The intervals reaching this function will either be
+  //both +ve, or both -ve
+  //So If one of them is +31 or -1, then the result will 
+  //be the other interval
+  if((knownOnesThis & SIGN_MASK) == interval::MAX) {
+    return other; 
+  } else if ((knownOnesOther & SIGN_MASK) == interval::MAX) {
+    return first;
+  }
+
+  //For all (+ve, +ve intervals) and (-ve, -ve intervals), 
+  //We can safely say, high <= max(hi, other.hi) because 
+  //this is & operation, so the leading/trailing zeros 
+  //will be lost 
+  
+  if (first.hi > other.hi) {
+    // Get the min high, then 'OR' with the knownZeros of that interval
+    // Now the high will have all the possibles ones present in the interval
+    // AND the result with the knownZeros of the second interval
+    //Sometimes 'OR' with the knownZeros bloats the high with values outside the
+    //range of the given interval. So just check that with the below case.
+    //If its bloated then replace the result with (min hi & resultKnownZeros) 
+    highest = ((other.hi | (knownZerosOther & SIGN_MASK)) & knownZerosThis);
+    highest = (highest > other.hi) ? (other.hi & resultKnownZeros) : highest;
+  } else {
+    highest = ((first.hi | (knownZerosThis & SIGN_MASK)) & knownZerosOther);
+    highest = (highest > first.hi) ? (first.hi & resultKnownZeros) : highest;
+  }
+
+
+  //resultknownOnes has 1 set only if all the elements in the 
+  //result interval has 1 set in that position. So its save to 
+  //set lowest to resultKnownOnes
+  return interval(resultKnownOnes, highest);
+
+}
+
+inline interval andOperWrapper(const interval &first, const interval &other) {
+  // One of them is a +ve interval and one is -ve 
+  //If so, change the negative interval to +ve interval and 
+  //call & operator again
+  //Since we know these -ve number will loose their
+  //sign while doing &, we can move this interval to the 
+  //positive boundary and get more precise LOW bound
+  if ((other.hi < 0) && (first.lo >= 0)) {
+    return andOper(first, interval((other.lo & SIGN_MASK), 
+                                   (other.hi & SIGN_MASK)));
+  }
+  else if ((first.hi < 0) && (other.lo >= 0)) {
+    return andOper(interval((first.lo & SIGN_MASK), 
+                            (first.hi & SIGN_MASK)), 
+                   other);
+  }
+
+  return andOper(first, other);
+}
+
+interval interval::operator&(const interval &other) {
 
   // Working with mixed intervals looses precision
   // So split them into +ve and -ve. 
@@ -57,75 +121,24 @@ interval interval::operator&(const interval &other) {
     interval this2 = interval(0, hi);
     interval other1 = interval(other.lo, -1);
     interval other2 = interval(0, other.hi);
-    return joinIntervals(this1 & other1, 
-            joinIntervals(this1 & other2,
-             joinIntervals(this2 & other1, this2 & other2)));
+    return joinIntervals(andOperWrapper(this1,  other1), 
+            joinIntervals(andOperWrapper(this1, other2),
+             joinIntervals(andOperWrapper(this2, other1), 
+                            andOperWrapper(this2, other2))));
     }
     else {
       interval this1 = interval(lo, -1);
       interval this2 = interval(0, hi);
-      return joinIntervals(this1 & other, this2 & other);
+      return joinIntervals(andOperWrapper(this1, other), 
+                            andOperWrapper(this2, other));
     }
   } 
   else if (isMixed(other)) {
     interval other1 = interval(other.lo, -1);
     interval other2 = interval(0, other.hi);
-      return joinIntervals(*this & other1, *this & other2);
+      return joinIntervals(andOperWrapper(*this, other1), 
+                            andOperWrapper(*this, other2));
   }
 
-  // One of them is a +ve interval and one is -ve 
-  //If so, change the negative interval to +ve interval and 
-  //call & operator again
-  //Since we know these -ve number will loose their
-  //sign while doing &, we can move this interval to the 
-  //positive boundary and get more precise LOW bound
-  if ((other.hi < 0) && (lo >= 0)) {
-    return (*this & interval(other.lo & SIGN_MASK, other.hi & SIGN_MASK));
-  }
-  else if ((hi < 0) && (other.lo >= 0)) {
-    return (interval(lo & SIGN_MASK, hi & SIGN_MASK) & other);
-  }
-
-  knownBits(*this, &knownOnesThis, &knownZerosThis);
-  knownBits(other, &knownOnesOther, &knownZerosOther);
-  resultKnownOnes = knownOnesThis & knownOnesOther; 
-  resultKnownZeros = knownZerosThis | knownZerosOther;
-  
-  //resultknownOnes has 1 set only if all the elements in the 
-  //result interval has 1 set in that position. So its save to 
-  //set lowest to resultKnownOnes
-  lowest = resultKnownOnes;
-
-  //For all (+ve, +ve intervals) and (-ve, -ve intervals), 
-  //We can safely say, high <= max(hi, other.hi) because 
-  //this is & operation, so the leading/trailing zeros 
-  //will be lost 
-  
-  if (hi > other.hi) {
-    // Get the min high, then 'OR' with the knownZeros of that interval
-    // Now the high will have all the possibles ones present in the interval
-    // AND the result with the knownZeros of the second interval
-    //Sometimes 'OR' with the knownZeros bloats the high with values outside the
-    //range of the given interval. So just check that with the below case.
-    //If its bloated then replace the result with (min hi & resultKnownZeros) 
-    highest = ((other.hi | (knownZerosOther & SIGN_MASK)) & knownZerosThis);
-    highest = (highest > other.hi) ? (other.hi & resultKnownZeros) : highest;
-  } else {
-    highest = ((hi | (knownZerosThis & SIGN_MASK)) & knownZerosOther);
-    highest = (highest > hi) ? (hi & resultKnownZeros) : highest;
-  }
-
-/*
-  if((knownOnesThis & SIGN_MASK) == 31) {
-    lowest = (knownOnesThis & other.lo) | (other.lo & SET_SIGN_MASK); 
-  } else if ((knownOnesOther & SIGN_MASK) == 31) {
-    lowest = (knownOnesOther & lo) | (lo & SET_SIGN_MASK);
-  } else {
-    return interval(lowest, highest);
-  }
-
-  if (lowest > highest) {
-    lowest = resultKnownOnes;
-  }*/
-  return interval(resultKnownOnes, highest);
+  return andOperWrapper(*this, other);
 }
